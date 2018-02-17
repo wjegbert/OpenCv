39628b4f295aebce911bc5f10489c6897072a610
William <wegbert@crimson.ua.edu>
2018-02-17 14:16:57 -0600

    Update opencvhellofaces.py

--- opencvhellofaces.py
+++ opencvhellofaces.py
@@ -6,8 +6,9 @@ Created on Sun Dec 31 13:40:11 2017
 """
 import numpy as np
 import cv2
-
+#captures feed from default webcam
 capture = cv2.VideoCapture(0)
+#initializes built-in face detection algo based on Haar cascades
 face_detect=cv2.CascadeClassifier(
         'C:\\Users\\William\\OpenCV\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_default.xml')
 
@@ -17,29 +18,24 @@ while(True):
     
     # Our operations on the frame come here
     if ret is True:
+        #convert to greyscale image
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #implement built-in face detection algorithm 
         faces=face_detect.detectMultiScale(gray, 1.3, 5)
-    #draw box around face? 
-      
+   
+        #draw box around face
         for (x,y,w,h) in faces:
-            print (str(x)+" "+str(y) + " "+ str(w) + " " +str(h))
+            #x,y,w,h are outputs from the face detection classifier 
             cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+            #creates region of interest around face
             roi_gray = gray[y:y+h, x:x+w]
-            roi_color = frame[y:y+h, x:x+w]
-            for (ex, ey, ew, eh) in roi_color:
-                
-
-                
-            
-
-    
-    #detect face?
+            roi_color = frame[y:y+h, x:x+w] 
   
         
-    # Display the resulting frame
+    # Display the resulting frame with face in box
     cv2.imshow('frame',frame)
 
-    # Closes all windows when 1 or q is pressed
+    # ends loop when q is pressed.
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
